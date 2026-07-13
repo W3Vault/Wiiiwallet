925877e8bd243f030d774a470358c990fea8fce8
5ha2s6 <tech@5ha2s6.com>
2026-07-13 22:20:05 +0100

    Update ElectrumX endpoint regression test

--- tests/unit/wiiicoin-network.test.ts
+++ tests/unit/wiiicoin-network.test.ts
@@ -97,6 +97,6 @@ describe('Wiiicoin network configuration', () => {
     expect(WIIICOIN_DERIVATION_PATHS.legacy).toBe("m/44'/9999'/0'");
     expect(WIIICOIN_DERIVATION_PATHS.wrappedSegwit).toBe("m/49'/9999'/0'");
     expect(WIIICOIN_DERIVATION_PATHS.nativeSegwit).toBe("m/84'/9999'/0'");
-    expect(WIIICOIN_ELECTRUM_SERVER).toEqual({ host: 'wiiicoin.io', tcp: 50001 });
+    expect(WIIICOIN_ELECTRUM_SERVER).toEqual({ host: 'etx1.wiiicoin.io', tcp: 50001 });
   });
 });
